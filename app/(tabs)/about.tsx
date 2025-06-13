30195746c99a7fdf3e63589d7a064f546d0e61df
abefqy <abefqy@gmail.com>
2025-06-13 23:19:29 +0800

    profile page

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,34 +1,3 @@
-// import { theme } from '@/constants/themes';
-// import { hp } from '@/helpers/common';
-// import { router } from 'expo-router';
-// import { Text, View, StyleSheet, Pressable } from 'react-native';
-
-
-// export default function AboutScreen() {
-//   return (
-//     <View style={styles.container}>
-//         <Pressable onPress={()=> router.push('../(auth)/welcome')}>
-//        <Text style={styles.text}>Logout</Text>        
-//         </Pressable>
-//         </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#25292e',
-//     justifyContent: 'flex-end',
-//     paddingBottom: hp(2),
-//     alignItems: 'center',
-//   },
-//   text: {
-//     color: '#ffd33d',
-//     fontSize: hp(2.6),
-//   },
-// });
-
-
 import React, { useState } from 'react';
 import { router } from 'expo-router';
 import {
